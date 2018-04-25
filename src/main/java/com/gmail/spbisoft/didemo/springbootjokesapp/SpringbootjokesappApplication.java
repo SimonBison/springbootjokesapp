78ae1dc212a1ec8b427b468025910a6b3cbf682e
sbi <szymon.bizon@hicron.com>
2018-04-25 15:16:18 +0200

    Initial commit

---
+++ src/main/java/com/gmail/spbisoft/didemo/springbootjokesapp/SpringbootjokesappApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.spbisoft.didemo.springbootjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjokesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootjokesappApplication.class, args);
+    }
+}
